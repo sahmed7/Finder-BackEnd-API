a0d61d94bb627d0a4d40529d110bf292d9896892
Shaheer Ahmed <sahmed3721@gmail.com>
2021-05-14 15:57:19 -0500

    updated createCity method

--- src/main/java/finderapi/demo/repository/CityRepository.java
+++ src/main/java/finderapi/demo/repository/CityRepository.java
@@ -4,10 +4,16 @@ import finderapi.demo.model.City;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
 
-    // City findByIdAndUserId(Long categoryId, Long userId);
-    //City findById(Long categoryId);
+    City findByIdAndUserId(Long categoryId, Long userId);
+
+    // find city by userId and by cityName
+    City findByUserIdAndName(Long userId, String cityName);
+
+    List<City> findByUserId(Long userId);
 
 }
--- src/main/java/finderapi/demo/service/CityService.java
+++ src/main/java/finderapi/demo/service/CityService.java
@@ -1,8 +1,12 @@
 package finderapi.demo.service;
 
+import finderapi.demo.exception.InformationExistException;
+import finderapi.demo.exception.InformationNotFoundException;
 import finderapi.demo.model.City;
 import finderapi.demo.repository.CityRepository;
+import finderapi.demo.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.List;
 
@@ -16,9 +20,31 @@ public class CityService {
     }
     
     public List<City> getCities() {
-        System.out.println("service calling getCities===>");
+        System.out.println("service calling getCities ==>");
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        System.out.println(userDetails.getUser().getId());
+        List<City> city = cityRepository.findByUserId(userDetails.getUser().getId());
+        if (city.isEmpty()) {
+            throw new InformationNotFoundException("no cities found for user id " + userDetails.getUser().getId());
+        } else {
+            return city;
+        }
     }
 
     public City createCity(City cityObject) {
+        System.out.println("service calling createCategory ==>");
+        MyUserDetails userDetails = (MyUserDetails) SecurityContextHolder.getContext().getAuthentication()
+                .getPrincipal();
+        City city = cityRepository.findByUserIdAndName(userDetails.getUser().getId(), cityObject.getName());
+
+        System.out.println("The city object is: ==============> " + city);
+        if(city != null) {
+            throw new InformationExistException("city with name " + cityObject.getName() + " already exists");
+        } else {
+            System.out.println("The city object is: ==============> " + cityObject);
+            cityObject.setUser(userDetails.getUser());
+            return cityRepository.save(cityObject);
+        }
     }
 }
