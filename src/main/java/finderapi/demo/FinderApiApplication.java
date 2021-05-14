ddc789322685c26a71d486840978e494b834f030
Shaheer Ahmed <sahmed3721@gmail.com>
2021-05-14 09:58:29 -0500

    first commit, barebones spring boot

---
+++ src/main/java/finderapi/demo/FinderApiApplication.java
@@ -0,0 +1,13 @@
+package finderapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinderApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinderApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/finderapi/demo/FinderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package finderapi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinderApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
