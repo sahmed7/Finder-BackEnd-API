a1549d356be9e5ab40fc4e7d136403e4ddbd515f
nleclaire <noah.leclaire@gmail.com>
2021-05-14 15:56:37 -0400

    Adding createUser to UserController and UserService

--- src/main/java/finderapi/demo/controller/UserController.java
+++ src/main/java/finderapi/demo/controller/UserController.java
@@ -1,4 +1,28 @@
 package finderapi.demo.controller;
 
+import finderapi.demo.model.User;
+import finderapi.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(path = "/users/register")
+    public User createUser(@RequestBody User userObject){return this.userService.createUser(userObject);}
+
+//    @PostMapping(path = "/users/login")
+//    public User loginUser(@RequestBody User userObject){return this.userService.loginUser(userObject);}
+
+
 }
--- src/main/java/finderapi/demo/service/UserService.java
+++ src/main/java/finderapi/demo/service/UserService.java
@@ -1,8 +1,10 @@
 package finderapi.demo.service;
 
+import finderapi.demo.exception.InformationExistException;
 import finderapi.demo.model.User;
 import finderapi.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,7 +13,25 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User findUserByEmailAddress(String emailAddress){
         return userRepository.findByEmailAddress(emailAddress);
     }
+
+    public User createUser(User userObject){
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+            if ((userRepository.existsByUserName(userObject.getUserName())))
+                throw new InformationExistException("User with Username '" + userObject.getUserName() +
+                        "' already exists");
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else {
+            throw new InformationExistException("User with email address: '" + userObject.getEmailAddress() +
+                    "' already exists");
+        }
+    }
+
+
 }
