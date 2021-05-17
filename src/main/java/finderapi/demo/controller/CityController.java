1ead5c3319ccce3612f7f1653b42b6b695b7b534
Shaheer Ahmed <sahmed3721@gmail.com>
2021-05-16 19:04:59 -0500

    added getSingleCity method, successful test in Postman

--- src/main/java/finderapi/demo/controller/CityController.java
+++ src/main/java/finderapi/demo/controller/CityController.java
@@ -2,11 +2,11 @@ package finderapi.demo.controller;
 
 import finderapi.demo.model.City;
 import finderapi.demo.service.CityService;
-import finderapi.demo.service.UtilityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping(path = "/api")
@@ -25,15 +25,25 @@ public class CityController {
         return "Hello World!";
     }
 
+    //http://localhost:9092/api/cities
+    @PostMapping("/cities")
+    public City createCity(@RequestBody City cityObject){
+        System.out.println("calling createCity ===>");
+        return cityService.createCity(cityObject);
+    }
+
+    //http://localhost:9092/api/cities
     @GetMapping("/cities")
     public List<City> getCities() {
         System.out.println("calling getCities ===>");
         return cityService.getCities();
     }
 
-    @PostMapping("/cities")
-    public City createCity(@RequestBody City cityObject){
-        System.out.println("calling createCity ===>");
-        return cityService.createCity(cityObject);
+    //http://localhost:9092/api/cities/1
+    @GetMapping(path = "/cities/{cityId}")
+    public Optional<City> getSingleCity(@PathVariable Long cityId) {
+        System.out.println("calling getSingleCity ==> ");
+        return cityService.getSingleCity(cityId);
     }
+
 }
--- src/main/java/finderapi/demo/service/CityService.java
+++ src/main/java/finderapi/demo/service/CityService.java
@@ -3,13 +3,13 @@ package finderapi.demo.service;
 import finderapi.demo.exception.InformationExistException;
 import finderapi.demo.exception.InformationNotFoundException;
 import finderapi.demo.model.City;
-import finderapi.demo.model.Restaurant;
 import finderapi.demo.model.User;
 import finderapi.demo.repository.CityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CityService {
@@ -23,6 +23,21 @@ public class CityService {
         this.cityRepository = cityRepository;
     }
 
+    // Create a city
+    public City createCity(City cityObject) {
+        System.out.println("service calling createCategory ==>");
+        User user = utility.getAuthenticatedUser();
+        City city = cityRepository.findByUserIdAndName(user.getId(), cityObject.getName());
+
+        if(city != null) {
+            throw new InformationExistException("city with name " + cityObject.getName() + " already exists");
+        } else {
+            cityObject.setUser(user);
+            return cityRepository.save(cityObject);
+        }
+    }
+
+    // Get all cities
     public List<City> getCities() {
         List<City> cities = cityRepository.findAll();
         if (cities.isEmpty()) {
@@ -32,16 +47,15 @@ public class CityService {
         }
     }
 
-    public City createCity(City cityObject) {
-        System.out.println("service calling createCategory ==>");
-        User user = utility.getAuthenticatedUser();
-        City city = cityRepository.findByUserIdAndName(user.getId(), cityObject.getName());
+    // Get a single city
+    public Optional<City> getSingleCity(Long cityId) {
+        System.out.println("service calling getCity ==>");
 
-        if(city != null) {
-            throw new InformationExistException("city with name " + cityObject.getName() + " already exists");
-        } else {
-        cityObject.setUser(user);
-        return cityRepository.save(cityObject);
+        Optional<City> city = cityRepository.findById(cityId);
+        if (city.isPresent()) {
+            return city;
+            } else {
+            throw new InformationNotFoundException("city with id " + cityId + " not found");
         }
     }
 }
